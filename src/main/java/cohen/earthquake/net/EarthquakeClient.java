65d52c0d06cee839aeba23a807fe98dd80483e23
Sarah Cohen <cohen.sarah591@gmail.com>
2018-09-13 20:55:30 -0400

    reversed to earlier commit

--- src/main/java/cohen/earthquake/net/EarthquakeClient.java
+++ src/main/java/cohen/earthquake/net/EarthquakeClient.java
@@ -1,44 +1,44 @@
-//package cohen.earthquake.net;
+package cohen.earthquake.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.google.gson.Gson;
+
+import cohen.earthquake.EarthquakeFeedModel;
+
+/**
+ * This is code to download the GeoJSON file directly from the web, not from a local file.
+ * @author sc
+ */
+//public class EarthquakeClient {
+//	public static void main(String[] args) throws IOException {
 //
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.net.HttpURLConnection;
-//import java.net.MalformedURLException;
-//import java.net.URL;
+//		//URL class encapsulates a url
+//		URL url = new URL("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson");
+//		//opens up a web-style connection. internally it uses a socket, but it's made in a way that we don't have to worry about http difficulties,
+//		//that would come up with using a socket.
+//		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 //
-//import com.google.gson.Gson;
+//		//how we read from our connection
+//		InputStream in = connection.getInputStream();
+//		//pipe into buffered reader to read easier
+//		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+//		//Gson and Earthquake feed to noget the actual json info
+//		Gson gson = new Gson();
+//		EarthquakeFeed feed = gson.fromJson(reader, EarthquakeFeed.class);
 //
-//import cohen.earthquake.EarthquakeFeedModel;
+//		System.out.println(
+//				feed.getFeatures()
+//				.stream()
+//				.filter(e -> e.getProperties().getMag() >= 5)
+//				.count());
 //
-///**
-// * This is code to download the GeoJSON file directly from the web, not from a local file.
-// * @author sc
-// */
-////public class EarthquakeClient {
-////	public static void main(String[] args) throws IOException {
-////
-////		//URL class encapsulates a url
-////		URL url = new URL("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson");
-////		//opens up a web-style connection. internally it uses a socket, but it's made in a way that we don't have to worry about http difficulties,
-////		//that would come up with using a socket.
-////		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-////
-////		//how we read from our connection
-////		InputStream in = connection.getInputStream();
-////		//pipe into buffered reader to read easier
-////		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-////		//Gson and Earthquake feed to noget the actual json info
-////		Gson gson = new Gson();
-////		EarthquakeFeed feed = gson.fromJson(reader, EarthquakeFeed.class);
-////
-////		System.out.println(
-////				feed.getFeatures()
-////				.stream()
-////				.filter(e -> e.getProperties().getMag() >= 5)
-////				.count());
-////
-////	}
-//
-////}
+//	}
+
+//}
--- src/main/java/cohen/earthquake/net/EarthquakeController.java
+++ src/main/java/cohen/earthquake/net/EarthquakeController.java
@@ -1,100 +1,99 @@
-//package cohen.earthquake.net;
-//
-////import java.awt.event.ActionEvent;
-//import java.util.Comparator;
-////import java.util.Optional;
-//import java.util.List;
-//import java.util.stream.Collector;
-//
-//import javax.swing.text.JTextComponent;
-//
-//import com.google.inject.Inject;
-//
-//import cohen.earthquake.Earthquake;
-//import cohen.earthquake.EarthquakeFeedModel;
-////import cohen.earthquake.EarthquakeProperties;
-//import retrofit2.Call;
-//import retrofit2.Callback;
-//import retrofit2.Response;
-//
-//public class EarthquakeController {
-//	private EarthquakeView view;
-//	private USGSEarthquakeService service;
-//
-//	@Inject
-//	EarthquakeController(EarthquakeView view, USGSEarthquakeService service) {
-//		this.view = view;
-//		this.service = service;
-//	}
-//
-//	public void refreshData() {
-//		requestMonth();
-//		requestWeek();
-//		requestDay();
-//		requestHour();
-//	}
-//
-//	void requestMonth() {
-//		requestEarthquakeFeed(service.getAllMonth(),
-//				view.getMonthMagText(),
-//				view.getMonthPlaceText());
-//	}
-//	private void requestWeek() {
-//		requestEarthquakeFeed(service.getAllWeek(),
-//				view.getWeekMagText(),
-//				view.getWeekPlaceText());
-//	}
-//	private void requestDay() {
-//		requestEarthquakeFeed(service.getAllDay(),
-//				view.getDayMagText(),
-//				view.getDayPlaceText());
-//	}
-//	private void requestHour() {
-//		requestEarthquakeFeed(service.getAllHour(),
-//				view.getHourMagText(),
-//				view.getHourPlaceText());
-//	}
-//
-//
-//
-//	private void requestEarthquakeFeed(Call<EarthquakeFeedModel> call,
-//									   final JTextComponent magnitudeField,
-//									   final JTextComponent placeField)
-//	{
-//		call.enqueue(new Callback<EarthquakeFeedModel>() {
-//			@Override
-//			public void onResponse(Call<EarthquakeFeedModel> call, Response<EarthquakeFeedModel> response) {
-//				EarthquakeFeedModel feed = response.body();
-//
-//				showLargestEarthquake(magnitudeField, placeField, feed);
-//
-//			}
-//
-//			@Override
-//			public void onFailure(Call<EarthquakeFeedModel> call, Throwable t) {
-//				t.printStackTrace();
-//
-//			}
-//
-//		});
-//
-//	}
-//
-//	void showLargestEarthquake(JTextComponent magnitudeField,
-//			JTextComponent placeField,
-//			EarthquakeFeedModel feed)
-//	{
-//		List<Earthquake> earthquakes = feed.getFeatures().stream()
-//				.filter(earthquake -> earthquake.getProperties().getMag() >= 3)
-//				.sorted(Comparator.comparing(Earthquake::getMagnitude).reversed())
-//				.limit(5)
-//				.collect(Collector.toList());
-//
-//		magnitudeField.setText(String.valueOf(largest.get().getProperties().getMag()));
-//		placeField.setText(String.valueOf(largest.get().getProperties().getPlace()));
-//		//viewProvider.get().setEarthquakes(earthquakes);
-//	}
-//
-//}
-//
-//
+package cohen.earthquake.net;
+
+import java.awt.event.ActionEvent;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collector;
+
+import javax.swing.text.JTextComponent;
+
+import com.google.inject.Inject;
+
+import cohen.earthquake.Earthquake;
+import cohen.earthquake.EarthquakeFeedModel;
+import cohen.earthquake.EarthquakeProperties;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EarthquakeController {
+	private EarthquakeView view;
+	private USGSEarthquakeService service;
+
+	@Inject
+	EarthquakeController(EarthquakeView view, USGSEarthquakeService service) {
+		this.view = view;
+		this.service = service;
+	}
+
+	public void refreshData() {
+		requestMonth();
+		requestWeek();
+		requestDay();
+		requestHour();
+	}
+
+	void requestMonth() {
+		requestEarthquakeFeed(service.getAllMonth(),
+				view.getMonthMagText(),
+				view.getMonthPlaceText());
+	}
+	private void requestWeek() {
+		requestEarthquakeFeed(service.getAllWeek(),
+				view.getWeekMagText(),
+				view.getWeekPlaceText());
+	}
+	private void requestDay() {
+		requestEarthquakeFeed(service.getAllDay(),
+				view.getDayMagText(),
+				view.getDayPlaceText());
+	}
+	private void requestHour() {
+		requestEarthquakeFeed(service.getAllHour(),
+				view.getHourMagText(),
+				view.getHourPlaceText());
+	}
+
+
+
+	private void requestEarthquakeFeed(Call<EarthquakeFeedModel> call,
+									   final JTextComponent magnitudeField,
+									   final JTextComponent placeField)
+	{
+		call.enqueue(new Callback<EarthquakeFeedModel>() {
+			@Override
+			public void onResponse(Call<EarthquakeFeedModel> call, Response<EarthquakeFeedModel> response) {
+				EarthquakeFeedModel feed = response.body();
+
+				showLargestEarthquake(magnitudeField, placeField, feed);
+
+			}
+
+			@Override
+			public void onFailure(Call<EarthquakeFeedModel> call, Throwable t) {
+				t.printStackTrace();
+
+			}
+
+		});
+
+	}
+
+	void showLargestEarthquake(JTextComponent magnitudeField,
+			JTextComponent placeField,
+			EarthquakeFeedModel feed)
+	{
+		List<Earthquake> earthquakes = feed.getFeatures().stream()
+				.filter(earthquake -> earthquake.getProperties().getMag() >= 3)
+				.sorted(Comparator.comparing(Earthquake::getMagnitude).reversed())
+				.limit(5)
+				.collect(Collector.toList());
+
+		magnitudeField.setText(String.valueOf(largest.get().getProperties().getMag()));
+		placeField.setText(String.valueOf(largest.get().getProperties().getPlace()));
+		//viewProvider.get().setEarthquakes(earthquakes);
+	}
+
+}
+
+
