0888a6f60a5cbbb14bef2486bbacc9d81062752b
Sarah E Cohen <scohen40@student.touro.edu>
2018-10-11 16:32:03 -0400

    began game play mechanics

---
+++ src/main/java/cohen/maze/mazeGUI/Man.java
@@ -0,0 +1,15 @@
+package cohen.maze.mazeGUI;
+
+public class Man {
+    //location
+    private int row;
+    private int column;
+
+    public Man(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+    //direction
+    //movement
+
+}
